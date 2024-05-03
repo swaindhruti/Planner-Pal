43a8bbfbdaaa8c9e50f0ebfd87ce947262055d11
Dhrutinandan Swain <dhrutiswainnandan@gmail.com>
2024-05-04 01:32:02 +0530

    feat: Add endpoint for creating and updating todos
    This commit adds a new endpoint for creating and updating todos in the backend. The  route now accepts POST requests to create a new todo and PUT requests to update the completion status of an existing todo. The code changes include updating the  file to handle these requests and integrating the  module for payload validation.

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { createTodo , updateTodo } = require('./input_validation');
+const todo = require('./db');
 const app = express();
 
 app.use(express.json());
 
-app.post('/todo', (req, res) => {
+app.post('/todo', async (req, res) => {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if(!parsedPayload.success){
@@ -12,10 +13,37 @@ app.post('/todo', (req, res) => {
             msg : 'Invalid Input',
         });
     }
+
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false,
+    })
+
+    res.json({
+        msg : "Todo Created"
+    })
 });
 
 app.get('/todos' , (req, res) => {
 });
 
-app.put('/completed' , (req, res) => {
+app.put('/completed' , async (req, res) => {
+    const createPayload = req.body;
+    const parsedPayload = updateTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        return res.status(411).json({
+            msg : 'Invalid Input',
+        });
+    }
+
+    await todo.updateOne({
+        _id : req.body.id
+    } , {
+        completed : true
+    });
+
+    res.json({
+        msg : "Todo Updated"
+    })
 });
