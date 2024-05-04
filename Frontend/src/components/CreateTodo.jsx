dcdfe2f9077f189ed54c3366ffea4a4f63432430
Dhrutinandan Swain <dhrutiswainnandan@gmail.com>
2024-05-04 10:56:06 +0530

    Added the frontend section for the project

---
+++ Frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,8 @@
+export function CreateTodo(){
+    return ( <div>
+        <input type="text" placeholder="title"/>
+        <input type="text" placeholder="description"/>
+        <button>Add a Todo</button>
+    </div>
+    )
+}
