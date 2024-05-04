618e5281825ced4107e0f1bd7aa6fef5d94b0c74
Dhrutinandan Swain <dhrutiswainnandan@gmail.com>
2024-05-04 16:27:42 +0530

    feat: Updated the CreateTodo component to hit the backend and used the usestate hhook of react for monitoring the change of input by user.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
-import { CreateTodo } from './components/createTodo.jsx'
+import "./App.css";
+import { CreateTodo } from "./components/createTodo.jsx";
+import { Todos } from "./components/todos.jsx";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  fetch("http://localhost:3000/todos")
+    .then((res) => res.json())
+    .then((data) => {
+      setTodos(data.todos);
+    });
+
   return (
     <div>
       <CreateTodo></CreateTodo>
+      <Todos todos={todos}></Todos>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/components/CreateTodo.jsx
+++ Frontend/src/components/CreateTodo.jsx
@@ -1,19 +1,53 @@
-export function CreateTodo(){
-    return <div>
-        <input style={{
-            padding: '10px',
-            margin: '10px',
-            width: '30%',
-            borderRadius: '5px',
-            border: '1px solid #ddd'
-        }} type="text" placeholder="title"/> <br />
-        <input style={{
-            padding: '10px',
-            margin: '10px',
-            width: '30%',
-            borderRadius: '5px',
-            border: '1px solid #ddd'
-        }} type="text" placeholder="description"/> <br />
-        <button>Add a Todo</button>
+import { useState } from "react";
+
+export function CreateTodo() {
+
+    const[title,setTitle] = useState("");
+    const[description,setDescription] = useState("");
+
+  return (
+    <div>
+      <input id="title"
+        style={{
+          padding: "10px",
+          margin: "10px",
+          width: "30%",
+          borderRadius: "5px",
+          border: "1px solid #ddd",
+        }}
+        type="text"
+        placeholder="title"
+        onChange={(e) => {setTitle(e.target.value)}}
+      />{" "}
+      <br />
+      <input id="description"
+        style={{
+          padding: "10px",
+          margin: "10px",
+          width: "30%",
+          borderRadius: "5px",
+          border: "1px solid #ddd",
+        }}
+        type="text"
+        placeholder="description"
+        onChange={(e) => {setDescription(e.target.value)}}
+      />{" "}
+      <br />
+      <button onClick={() => {
+        fetch("http://localhost:3000/todo", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: title,
+            description: description
+          }),
+        }).then((res) => {
+            res.json();
+            alert("Todo Added Successfully");
+        });
+      }}>Add a Todo</button>
     </div>
+  );
 }
